83ce529393b74193a0ee4cc55e4dde9daf42e2d9
Alain Richel Rodríguez Blazquez <alainrichell@gmail.com>
2024-12-13 22:16:55 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api' // Update this with your actual API endpoint
+  apiUrl: 'https://alainrichell.pythonanywhere.com/api' // Update this with your actual API endpoint
 };
